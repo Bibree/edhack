b8d46b2aa44121a61e31bd3cc2929f83ae2f7a64
Tony Seing <tony.seing@gmail.com>
2014-10-18 19:14:55 -0400

    adds twitter libs and routes

--- server.rb
+++ server.rb
@@ -4,9 +4,34 @@ require "koala"
 require "pry"
 require "firebase"
 require "./classforms.rb"
+require 'omniauth-twitter'
 
 configure do
   enable :sessions
+  use OmniAuth::Builder do
+    provider :twitter, 'BSuhlPmwPyPpQXVx486tWWnfn', 'fMdOju0tjM9lvovnUidJqwdNxM06QDmW9UCB4VWhHnlRXORZjm'
+  end
+
+end
+
+
+helpers do
+  # define a current_user method, so we can be sure if an user is
+  # authenticated
+  def current_user
+    !session[:uid].nil?
+  end
+end
+
+before do
+  # we do not want to redirect to twitter when the path info starts
+  # with /auth/
+  pass if request.path_info =~ /^\/auth\//
+
+  # /auth/twitter is captured by omniauth:
+    # when the path info matches /auth/twitter, omniauth will redirect
+  # to twitter
+  redirect to('/auth/twitter') unless current_user
 end
 
 
@@ -39,6 +64,18 @@ end
 get '/class/add/:id' do
   
   erb :"add_classes.html.erb"
+end
+
+get '/auth/twitter/callback' do
+  # probably you will need to create a user in the database too...
+  session[:uid] = env['omniauth.auth']['uid']
+  # this is the main endpoint to your application
+  redirect to('/')
+end
+
+get '/auth/failure' do
+  # omniauth redirects to /auth/failure when it encounters a problem
+  # so you can implement this as you please
 end
 
 post '/class/add/:id' do
