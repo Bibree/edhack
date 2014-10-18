222cf72b8c22c8cf52b977b0bac22a47ea37ec54
Legenetic <coellopatrick@gmail.com>
2014-10-18 18:12:49 -0400

    Create classform.rb
    still in progress

---
+++ classform.rb
@@ -0,0 +1,42 @@
+#
+#
+#
+#
+
+
+enable : server 
+
+get '/' do
+    #userid = params[:userid]
+    #ttt = params[:teachertwitterhandle]
+    #classhashtag = params[:]
+end    
+  
+def add_class(user_id, teacher_handle, class_hashtag) do
+  user_id = user +"/"
+  
+  checkuser = class hashtag.get()
+  if userid = access_token.params[:] #true    .debug:successful
+  
+  else.false    #.debug unsuccessful
+  
+  
+  @user_id, @teacher_handle, @class_hashtag = user_id, teacher_hnadle, class_hashtag
+    
+end
+
+get 'teach_feed' do
+  erb :
+end
+
+def get_posts (user_id)
+  session[:user_id] =
+  if.checkuser = firebase.get(userid)
+    checkclass = firebase.get(class_hashtag)
+      firebase = Firebase::Client.new(base_uri)
+
+  then.@request_ 
+
+
+end
+
